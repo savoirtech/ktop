76b86abfebe230b0501e1574ff6aecd330a9e64b
jgoodyear <jgoodyear@apache.org>
2014-10-19 12:14:52 -0230

    Updated readme, and implemented simple console updater

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ ktop
 ====
 
 Karaf Top Command
+
+
+To build, invoke:
+ 
+ mvn install
+
+
+To install in Karaf, invoke from console:
+
+ install -s mvn:com.savoirtech.karaf.commands/ktop
+
+
+To execute command on Karaf, invoke:
+
+ aetos:ktop
+
+
+To exit ktop, press control + c
--- src/main/java/com/savoirtech/karaf/commands/ktop.java
+++ src/main/java/com/savoirtech/karaf/commands/ktop.java
@@ -1,24 +1,38 @@
 
 package com.savoirtech.karaf.commands;
 
-import org.apache.karaf.shell.console.OsgiCommandSupport;
+import java.io.IOException;
+
+import org.apache.karaf.shell.commands.Argument;
 import org.apache.karaf.shell.commands.Command;
 import org.apache.karaf.shell.commands.Option;
-import org.apache.karaf.shell.commands.Argument;
+import org.apache.karaf.shell.console.AbstractAction;
 
 @Command(scope = "aetos", name = "ktop", description = "Karaf Top Command")
-public class ktop extends OsgiCommandSupport {
+public class ktop extends AbstractAction {
 
-    @Option(name = "-o", aliases = { "--option" }, description = "An option to the command", required = false, multiValued = false)
-    private String option;
-
-    @Argument(name = "argument", description = "Argument to the command", required = false, multiValued = false)
-    private String argument;
+    private static final int DEFAULT_SLEEP_INTERVAL = 200;
 
     protected Object doExecute() throws Exception {
-         System.out.println("Executing command ktop");
-         System.out.println("Option: " + option);
-         System.out.println("Argument: " + argument);
-         return null;
+        try {
+            ktop();
+        } catch (IOException e) {
+            //Ignore
+        }
+        return null;
+    }
+
+    private void ktop() throws InterruptedException, IOException {
+
+        // Continously update stats to console.
+        while (true) {
+            Thread.sleep(DEFAULT_SLEEP_INTERVAL);
+            //Clear console, then print JVM stats
+            System.out.print("\33[2J");
+            System.out.flush();
+            System.out.print("\33[1;1H");
+            System.out.flush();
+            System.out.println("Hello World " + System.currentTimeMillis());
+        }
     }
 }

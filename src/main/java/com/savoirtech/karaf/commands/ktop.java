4dbc64817b71dae18e6b967dea96bd256948a857
jgoodyear <jgoodyear@apache.org>
2014-10-20 07:55:53 -0230

    sample code for displaying top threads

--- src/main/java/com/savoirtech/karaf/commands/ktop.java
+++ src/main/java/com/savoirtech/karaf/commands/ktop.java
@@ -9,6 +9,18 @@ import java.lang.management.OperatingSystemMXBean;
 import java.lang.management.RuntimeMXBean;
 import java.lang.management.ThreadMXBean;
 
+import java.lang.management.ThreadInfo;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.LinkedHashMap;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.io.IOException;
 
 import org.apache.karaf.shell.commands.Argument;
@@ -20,6 +32,12 @@ import org.apache.karaf.shell.console.AbstractAction;
 public class ktop extends AbstractAction {
 
     private static final int DEFAULT_SLEEP_INTERVAL = 200;
+ 
+    private boolean         sortByTotalCPU_         = false;
+    private int             numberOfDisplayedThreads_ = 30;
+    private boolean         displayedThreadLimit_     = true;
+    private Map<Long, Long> previousThreadCPUMillis = new HashMap<Long, Long>();
+ 
 
     protected Object doExecute() throws Exception {
         try {
@@ -46,24 +64,120 @@ public class ktop extends AbstractAction {
             System.out.flush();
             System.out.print("\33[1;1H");
             System.out.flush();
-            System.out.println("ktop:");
-            System.out.println("Uptime " + runtime.getUptime());
 
-            System.out.println("Threads");
-            System.out.println("Live threads " + threads.getThreadCount());
-            System.out.println("Daemon threads " + threads.getDaemonThreadCount());
-            System.out.println("Peak " + threads.getPeakThreadCount());
-            System.out.println("Total started " + threads.getTotalStartedThreadCount());
-
-            System.out.println("Memory");
-            System.out.println("Current heap size " + mem.getHeapMemoryUsage().getUsed());
-            System.out.println("Maximum heap size " + mem.getHeapMemoryUsage().getMax());
-            System.out.println("Committed heap size " +  mem.getHeapMemoryUsage().getCommitted());
-
-            System.out.println("Classes");
-            System.out.println("Current classes loaded " + cl.getLoadedClassCount());
-            System.out.println("Total classes loaded " + cl.getTotalLoadedClassCount());
-            System.out.println("Total classes unloaded " + cl.getUnloadedClassCount());
+            System.out.printf(" ktop - %8tT, %6s, %2d cpus, %15.15s",  
+                               new Date(), os.getArch(), os.getAvailableProcessors(), os.getName() + " "  + os.getVersion());
+            System.out.printf(", load avg %3.2f%n", os.getSystemLoadAverage());
+            System.out.println("\n");
+            System.out.println("==========================================================================================");
+            System.out.printf("  TID   NAME                                              STATE    CPU  TOTALCPU BLOCKEDBY%n");
+            printTopThreads(threads);
+            System.out.println("==========================================================================================");
         }
+    }
+
+    private void printTopThreads(ThreadMXBean threads) {
+        // Print top ten threads
+        if (threads.isThreadCpuTimeSupported()) {
+            // This JVM supports telling us thread stats!
+            Map<Long, Long> newThreadCPUMillis = new HashMap<Long, Long>();
+
+            Map<Long, Long> cpuTimeMap = new TreeMap<Long, Long>();
+
+            for (Long tid : threads.getAllThreadIds()) {
+                long threadCpuTime = threads.getThreadCpuTime(tid);
+                long deltaThreadCpuTime = 0;
+                if (previousThreadCPUMillis.containsKey(tid))
+                {
+                    deltaThreadCpuTime = threadCpuTime - previousThreadCPUMillis.get(tid);
+
+                    cpuTimeMap.put(tid, deltaThreadCpuTime);
+                }
+                newThreadCPUMillis.put(tid, threadCpuTime);
+            }
+
+            cpuTimeMap = sortByValue(cpuTimeMap, true);
+
+            int displayedThreads = 0;
+            for (Long tid : cpuTimeMap.keySet()) {
+                ThreadInfo info = threads.getThreadInfo(tid);
+                displayedThreads++;
+                if (displayedThreads > numberOfDisplayedThreads_
+                   && displayedThreadLimit_)
+                {
+                   break;
+                }
+                if (info != null) {
+                    System.out.printf(" %6d %-40s  %13s %5.2f%%    %5.2f%% %5s %n",
+                                      tid,
+                                      leftStr(info.getThreadName(), 40),
+                                      info.getThreadState(),
+                                      10.0,
+                                      10.0, getBlockedThread(info));
+                                      //getThreadCPUUtilization(cpuTimeMap.get(tid), vmInfo_.getDeltaUptime()),
+                                      //getThreadCPUUtilization(threads.getThreadCpuTime(tid), 
+                                      //                        vmInfo_.getProxyClient().getProcessCpuTime(), 1), 
+                                      //                        getBlockedThread(info));
+                }
+            }
+            if (newThreadCPUMillis.size() >= numberOfDisplayedThreads_
+                && displayedThreadLimit_)
+            {
+
+            System.out.printf(" Note: Only top %d threads (according cpu load) are shown!",
+                              numberOfDisplayedThreads_);
+            System.out.println("");
+            }
+            previousThreadCPUMillis = newThreadCPUMillis;
+        } else {
+            System.out.printf("%n -Thread CPU telemetries are not available on the monitored jvm/platform-%n");
+        }
+    }
+
+
+    public Map sortByValue(Map map, boolean reverse) {
+        List list = new LinkedList(map.entrySet());
+        Collections.sort(list, new Comparator() {
+
+            @Override
+            public int compare(Object o1, Object o2) {
+                return ((Comparable) ((Map.Entry) (o1)).getValue()).compareTo(((Map.Entry) (o2)).getValue());
+            }
+        });
+
+        if (reverse) {
+            Collections.reverse(list);
+        }
+
+        Map result = new LinkedHashMap();
+        for (Iterator it = list.iterator(); it.hasNext();) {
+            Map.Entry entry = (Map.Entry) it.next();
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+
+    public String leftStr(String str, int length) {
+        return str.substring(0, Math.min(str.length(), length));
+    }
+
+    private String getBlockedThread(ThreadInfo info) {
+        if (info.getLockOwnerId() >= 0) {
+            return "" + info.getLockOwnerId();
+        } else {
+            return "";
+        }
+    }
+
+
+    private double getThreadCPUUtilization(long deltaThreadCpuTime, long totalTime) {
+        return getThreadCPUUtilization(deltaThreadCpuTime, totalTime, 1000 * 1000);
+    }
+
+    private double getThreadCPUUtilization(long deltaThreadCpuTime,long totalTime, double factor) {
+        if (totalTime == 0) {
+            return 0;
+        }
+        return deltaThreadCpuTime / factor / totalTime * 100d;
     }
 }
